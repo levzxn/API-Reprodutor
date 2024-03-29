2af96820b9251a94365df15d598506e6238af04d
Lucas4i20 <127855437+Lucas4i20@users.noreply.github.com>
2024-03-29 12:59:00 -0300

    Duração de audio formatada

--- API-reprodutor/reprodutor/api.py
+++ API-reprodutor/reprodutor/api.py
@@ -8,5 +8,5 @@ api = NinjaAPI()
 def musica(request):
     urlServidor = 'http://127.0.0.1:8000'
     musicas = Musica.objects.all()
-    jsonMusicas = [{'titulo': i.titulo, 'audio': urlServidor + i.audio.url,'capa':urlServidor + i.capa.url} for i in musicas]
+    jsonMusicas = [{'titulo': i.titulo, 'audio': urlServidor + i.audio.url,'capa':urlServidor + i.capa.url,'duracao':i.duracao_formatada} for i in musicas]
     return jsonMusicas
